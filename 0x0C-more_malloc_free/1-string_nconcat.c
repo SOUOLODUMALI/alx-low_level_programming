f986a904b11ed519db4bcc7ec9198d94d6bd1857
SOUOLODUMALI <bedjick@gmail.com>
2023-03-17 17:12:16 -0700

    entask

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,59 +1,29 @@
+#include "main.h"
 #include <stdlib.h>
-
 /**
- * _strlen - returns the length of a string
- * @string: string to be measured
- * Return: length of string
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
+ *
+ * Return: pointer to the newly created array.
+ * if man > mix, returns NULL.
+ * if malloc fails, returns NULL.
  */
-int _strlen(char *string)
+int *array_range(int min, int max)
 {
-int i;
-for (i = 0; string[i] != '\0'; i++)
-return (i);
-}
+	int *ar;
+	int i;
 
-/**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: number of bytes of s2 to concatenate
- * Return: pointer to new string
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-char *ptr;
-int num, len, i, j;
-num = n;
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
-}
-if (num < 0)
-{
-return (NULL);
-}
-if (num >= _strlen(s2))
-{
-num = _strlen(s2);
-}
-len = _strlen(s1) + num;
-ptr = malloc(sizeof(*ptr) * len);
-if (ptr == NULL)
-{
-return (NULL);
-}
-for (i = 0; s1[i] != '\0'; i++)
-{
-ptr[i] = s1[i];
-}
-for (j = 0; j < num; j++)
-{
-ptr[i + j] = s2[j];
-}
-ptr[i + j] = '\0';
-return (ptr);
+	if (min > max)
+		return (NULL);
+
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++, min++)
+		ar[i] = min;
+
+	return (ar);
 }
