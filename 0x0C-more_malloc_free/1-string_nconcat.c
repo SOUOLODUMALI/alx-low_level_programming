5fab07a69e0e6e7c7c35c5d359116b5cb2e42744
SOUOLODUMALI <bedjick@gmail.com>
2023-03-18 00:22:22 -0700

    anewcommit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,29 +1,44 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * array_range - creates an array of integers.
- * @min: minimum value.
- * @max: maximum value.
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
  *
- * Return: pointer to the newly created array.
- * if man > mix, returns NULL.
- * if malloc fails, returns NULL.
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
-int *array_range(int min, int max)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int *ar;
-	int i;
+	char *concat;
+	unsigned int len = n, index;
 
-	if (min > max)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
 
-	ar = malloc(sizeof(*ar) * ((max - min) + 1));
+	len = 0;
 
-	if (ar == NULL)
-		return (NULL);
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
 
-	for (i = 0; min <= max; i++, min++)
-		ar[i] = min;
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
 
-	return (ar);
+	concat[len] = '\0';
+
+	return (concat);
 }
