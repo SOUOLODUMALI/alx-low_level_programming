304c851ac323c067d953fbbd3dc0310bf4514bcf
SOUOLODUMALI <bedjick@gmail.com>
2023-03-21 17:19:53 -0700

    end

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
-#include "dog.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+};
+ 
+
+typedef struct dog dog_t;
+ 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+ 
+#endif
